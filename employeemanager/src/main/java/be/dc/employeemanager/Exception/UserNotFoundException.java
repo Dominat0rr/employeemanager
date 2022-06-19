e4e7733b1b8c76a7f3695db8cd593e75d6ccfa52
Dominat0rr <claermandominik@gmail.com>
2022-06-20 00:59:31 +0200

    Create UserNotFoundException.java

---
+++ employeemanager/src/main/java/be/dc/employeemanager/Exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package be.dc.employeemanager.Exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
