3073720aacbd028a1604a6e74081828a302fb718
Dominat0rr <claermandominik@gmail.com>
2022-06-20 00:56:20 +0200

    Update EmployeeRepository.java

--- employeemanager/src/main/java/be/dc/employeemanager/Repository/EmployeeRepository.java
+++ employeemanager/src/main/java/be/dc/employeemanager/Repository/EmployeeRepository.java
@@ -4,5 +4,5 @@ import be.dc.employeemanager.Model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    void deleteEmployeeById(Long id);
 }
