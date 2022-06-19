e7630fc57a1a3f21f2ab4796519cf6331f165fe4
Dominat0rr <claermandominik@gmail.com>
2022-06-20 00:06:41 +0200

    Create EmployeeRepository.java

---
+++ employeemanager/src/main/java/be/dc/employeemanager/Repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package be.dc.employeemanager.Repository;
+
+import be.dc.employeemanager.Model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
