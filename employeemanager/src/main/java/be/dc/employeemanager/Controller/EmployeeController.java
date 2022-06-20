7c63ae46fe47f306193ca492095c1a9bf354a734
Dominat0rr <claermandominik@gmail.com>
2022-06-20 11:17:13 +0200

    Update EmployeeController.java

--- employeemanager/src/main/java/be/dc/employeemanager/Controller/EmployeeController.java
+++ employeemanager/src/main/java/be/dc/employeemanager/Controller/EmployeeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api/employee")
 public class EmployeeController {
     private final EmployeeService service;
 
