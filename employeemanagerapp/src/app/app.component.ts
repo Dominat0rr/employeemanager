d3bfc523f99265a35dd7258881832b235752e275
Dominat0rr <claermandominik@gmail.com>
2022-06-20 13:32:49 +0200

    Update app.component.ts

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -55,6 +55,27 @@ export class AppComponent implements OnInit {
     button.click();
   }
 
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+
+    this.employees!.forEach(employee => {
+      // -1 = it is not present
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    });
+
+    this.employees = results;
+
+    if (/*results.length === 0 ||*/ !key) {
+      this.getEmployees();
+    }
+  }
+
   public onAddEmployee(addForm: NgForm): void {
     document.getElementById("add-employee-form")?.click();
     this.employeeService.addEmployee(addForm.value).subscribe(
